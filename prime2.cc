86ce745dcc5e4e4c3856f02518d4991d5e32747c
Pablourquia <alu0101398327@ull.edu.es>
2020-11-17 17:38:35 +0000

    nuevos cambios

---
+++ prime2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+void usage(int argc, char *argv[]) {
+    string kHelpText {"Este programa le pide que al ejecutar el programa introduzca un digito y te dira cual es el numero primo en su posicion"};
+    if (argc !=2){
+        cout << argv[0] << " : Falta un numero natural como parametro" << endl;
+        cout << "Pruebe " << argv[0] << " --help para mas informacion" << endl;
+        exit (EXIT_SUCCESS);
+    }
+    string parameter = {argv[1]};
+    if (parameter == "--help"){
+        cout << kHelpText << endl;
+        exit (EXIT_SUCCESS);
+    }
+}
+
+
+vector<int> Primes (int parameter){
+    vector <int> primes;
+    do {
+        int dividers = 0;
+        for(int i = 2; i < 1000; i++){
+            for(int j = 1 ; j <= i; j++){
+                if(i % j == 0){
+                    dividers += 1;
+                }
+            }
+                if(dividers == 2){
+                    primes.push_back(i);
+                }
+        }
+    } while (primes [parameter] != 0);
+    
+    return primes;
+}
+
+int main(int argc,char *argv[]){
+
+    int parameter = atoi (argv[1]);
+    vector<int> final = Primes(parameter);
+    cout << final[parameter] << endl;
+    return 0;
+}
